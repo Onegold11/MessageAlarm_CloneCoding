7032dfe9c57b0879dde8c7dba37fb3305da6ccdd
woojin.cheon <woojin.cheon@formalworks.com>
2021-09-17 15:27:34 +0900

    [feature] 알림 기능 추가

--- app/src/main/java/com/clonecoding/clouldmessagealarm/MainActivity.kt
+++ app/src/main/java/com/clonecoding/clouldmessagealarm/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.clonecoding.clouldmessagealarm
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,5 +26,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.initFirebase()
+        this.updateResult()
+    }
+
+    /**
+     * Firebase 초기화
+     */
+    private fun initFirebase() {
+
+        FirebaseMessaging.getInstance().token
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    this.firebaseTextview.text = it.result
+                }
+            }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateResult(isNewIntent: Boolean = false) {
+
+        this.resultTextView.text = (intent.getStringExtra("notificationType") ?: "앱 런처") +
+                if (isNewIntent) {
+                    "(으)로 갱신했습니다."
+                } else {
+                    "(으)로 실행했습니다."
+                }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        setIntent(intent)
+        this.updateResult(true)
     }
 }
