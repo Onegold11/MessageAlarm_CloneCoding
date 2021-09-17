015a1ca18df26c1e88aedd4f8110cf4cabb583b0
woojin.cheon <woojin.cheon@formalworks.com>
2021-09-17 11:14:34 +0900

    [feature] UI 추가

--- app/src/main/java/com/clonecoding/clouldmessagealarm/MainActivity.kt
+++ app/src/main/java/com/clonecoding/clouldmessagealarm/MainActivity.kt
@@ -2,8 +2,24 @@ package com.clonecoding.clouldmessagealarm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    /**
+     * Result text view
+     */
+    private val resultTextView: TextView by lazy {
+        findViewById(R.id.resultTextView)
+    }
+
+    /**
+     * Firebase text view
+     */
+    private val firebaseTextview: TextView by lazy {
+        findViewById(R.id.firebaseTokenTextView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
